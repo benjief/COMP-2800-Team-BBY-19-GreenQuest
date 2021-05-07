398be486c5d0ef1f70bcfd087f5d33e1d1175f89
Benjie Friedman <benjiefriedman@gmail.com>
2021-05-06 22:19:51 -0700

    When a user enters a group name and clicks 'Submit' in educator-create-group.html, a group is written to Firestore with that name. So the very basic 'add a group' functionality for educators has been implemented. In addition, if a user doesn't enter a group name, an error message pops up. If they do enter a group name, a success message pops up.

--- scripts/educator-create-group.js
+++ scripts/educator-create-group.js
@@ -1,11 +1,13 @@
 // JS for educator-create-group.js
 
+var noInput = false;
+
 /**
  * Implement a character limit counter
  * 
  * @param {*} field - DOM-element that characters are being counted in
  * @param {*} field2 - ID of the DOM-element displaying the number of characters remaining
- * @param {*} maxlimit - maximum number of characters allowed in "field"
+ * @param {*} maxlimit - Maximum number of characters allowed in "field"
  * @returns - false if the character limit has been exceeded
  */
 function charCounter(field, field2, maxlimit) {
@@ -19,28 +21,65 @@ function charCounter(field, field2, maxlimit) {
 }
 
 /**
+ * Write a group to Firestore.
+ * 
+ * @param groupName - String containing the name of the group to be created
+ */
+function addGroup(groupName) {
+    db.collection("Groups").doc(groupName).set({
+        id: groupName,
+        Owner_Name: firebase.auth().currentUser.displayName,
+        Owner_Email: firebase.auth().currentUser.email,
+        Date_Created: firebase.firestore.FieldValue.serverTimestamp()
+    })
+        .then(() => {
+            console.log("Document successfully written!");
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+}
+
+/**
+ * Make sure the user has input a group name
+ * 
+ * @param groupName - String containing the name of the group to be created
+ */
+function checkInput(groupName) {
+    if (groupName == null || groupName === "") {
+        noInput = true;
+        $("#feedback").html("Please enter a group name");
+        $("#feedback").css({
+            color: "red"
+        });
+        $("#feedback").show(0);
+        $("#feedback").fadeOut(2500);
+    } else {
+        noInput = false;
+    }
+}
+
+/**
  * Deal with submission click in the appropriate manner
  */
 function onClickSubmit() {
-    // Grab comment
-    console.log(document.getElementById("comment_box").value);
-    var feedback = document.getElementById("feedback");
-    checkSelection();
-    if (!noStoreSelected) {
-        checkRating();
-    }
-    // Add or modify a review if selections are valid
-    if (!noStoreSelected && !noRating) {
-        addReview(store);
+    // Store group name in a variable
+    console.log(document.getElementById("group-name").value);
+    let groupName = document.getElementById("group-name").value;
+    checkInput(groupName);
+    if (noInput == false) {
+        // Add group to Firestore
+        addGroup(groupName);
         // Display success message and direct users back to the main page
-        document.getElementById("feedback").innerHTML = "Thanks for your feedback!";
+        document.getElementById("feedback").innerHTML = "Success! Please wait...";
         $(feedback).css({
             color: "green"
         });
         $(feedback).show(0);
         $(feedback).fadeOut(2500);
         setTimeout(function () {
-            location.href = "/web/member/main.html"
+            location.href = "#";
         }, 2300);
     }
+
 }
