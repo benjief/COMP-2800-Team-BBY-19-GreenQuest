0093672ea746875a58cc523b2078c9f886d709d9
Benjie Friedman <benjiefriedman@gmail.com>
2021-05-07 12:44:51 -0700

    educator-remove-students.html is now fully functional.

--- scripts/educator-remove-students.js
+++ scripts/educator-remove-students.js
@@ -1,7 +1,6 @@
 // JS for educator-remove-students.js
 
 // Create empty lists to house student names
-var studentNames = [];
 var currentStudents = [];
 
 // Pull group name from URL and display it in the DOM
@@ -14,22 +13,22 @@ $(".page-heading").html("Add Students to " + groupName);
  * Appends a list of student names (along with a "+" icon) to the DOM.
  */
 function populateStudentList() {
-    console.log(studentNames);
-    for (var i = 0; i < 10; i++) {
+    console.log(currentStudents);
+    for (var i = 0; i < currentStudents.length; i++) {
         let studentContainer = "<div class='student-container' id='student-container-" + i + "'></div>";
         $(".student-list").append(studentContainer);
-        let studentName = "<p class='student-name' id='student-name-" + i + "'>" + studentNames[i] + "</p>";
+        let studentName = "<p class='student-name' id='student-name-" + i + "'>" + currentStudents[i] + "</p>";
         $("#student-container-" + i).append(studentName);
         let iconContainer = "<div class='icon-container' id='icon-container-" + i + "'></div>";
         $("#student-container-" + i).append(iconContainer);
-        let plusIcon = "<img src='/img/add_icon.png' class='icon' id='plus-icon-" + i
-            + "' onclick='addStudent()'>";
+        let plusIcon = "<img src='/img/remove_icon.png' class='icon' id='minus-icon-" + i
+            + "' onclick='removeStudent()'>";
         $("#icon-container-" + i).append(plusIcon);
     }
 }
 
 /** 
- * Gets the names of students who are already in this group.
+* Reads students' names from Firestore and puts them into an array if they aren't already in this group.
  */
 function getCurrentStudents() {
     db.collection("Groups").doc(groupName).collection("Students")
@@ -38,29 +37,34 @@ function getCurrentStudents() {
         querySnapshot.forEach((doc) => {
             currentStudents.push(doc.data().id);
         });
-        getStudents();
+        populateStudentList();
     })
 }
 
 /**
- * Reads students' names from Firestore and puts them into an array if they aren't already in this group.
+ * Removes the chosen student from a collection in Firestore (nested under the group being removed from).
+ * Also changes the "-" icon beside a student to a "+" icon and allows that student to be subsequently
+ * re-added to the group in question.
  */
-function getStudents() {
-    console.log(currentStudents);
-    db.collection("Users")
-        .where("User_Type", "==", "student")
-        .get()
-        .then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                if (!currentStudents.includes(doc.data().Name)) {
-                    studentNames.push(doc.data().Name);
-                }
-            });
-            populateStudentList();
-        })
-        .catch((error) => {
-            console.log("Error getting students: ", error);
-        });
+ function removeStudent(event) {
+    $(document).click(function (event) {
+        let index = $(event.target).attr("id");
+        // Extract index from event id
+        index = index.match(/\d+/);
+        // Replace "remove" icon with an "add" icon
+        $(event.target).attr("src", "/img/add_icon.png");
+        // Get "add" icon to call addStudent()
+        $(event.target).attr("onclick", "addStudent()");
+        // Remove student from collection in Firestore
+        let studentToRemove = currentStudents[index];
+        db.collection("Groups").doc(groupName).collection("Students").doc(studentToRemove).delete()
+            .then(() => {
+                console.log("Student successfully removed!");
+            })
+            .catch((error) => {
+                console.error("Error removing student: ", error);
+            })
+    });
 }
 
 /**
@@ -78,7 +82,7 @@ function addStudent() {
         // Get "remove" icon to call removeStudent()
         $(event.target).attr("onclick", "removeStudent()");
         // Add student to a collection in Firestore (nested under the group being added to)
-        let studentToAdd = studentNames[index];
+        let studentToAdd = currentStudents[index];
         db.collection("Groups").doc(groupName).collection("Students").doc(studentToAdd).set({
             id: studentToAdd
         })
@@ -88,32 +92,6 @@ function addStudent() {
             .catch((error) => {
                 console.error("Error adding student: ", error);
             });
-    });
-}
-
-/**
- * Removes the chosen student from a collection in Firestore (nested under the group being removed from).
- * Also changes the "-" icon beside a student to a "+" icon and allows that student to be subsequently
- * re-added to the group in question.
- */
-function removeStudent(event) {
-    $(document).click(function (event) {
-        let index = $(event.target).attr("id");
-        // Extract index from event id
-        index = index.match(/\d+/);
-        // Replace "remove" icon with an "add" icon
-        $(event.target).attr("src", "/img/add_icon.png");
-        // Get "add" icon to call addStudent()
-        $(event.target).attr("onclick", "addStudent()");
-        // Remove student from collection in Firestore
-        let studentToRemove = studentNames[index];
-        db.collection("Groups").doc(groupName).collection("Students").doc(studentToRemove).delete()
-            .then(() => {
-                console.log("Student successfully removed!");
-            })
-            .catch((error) => {
-                console.error("Error removing student: ", error);
-            })
     });
 }
 
