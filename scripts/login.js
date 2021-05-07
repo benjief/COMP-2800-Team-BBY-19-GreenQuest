cb01515b5253978e07471acc53b37339c491625a
Benjie Friedman <benjiefriedman@gmail.com>
2021-05-06 18:45:43 -0700

    Added functionality to record what type of user is logging in (educator or student) and redirect users accordingly (also depending on if they are new users or not).

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,15 @@
 // JS for login.html
+
+// Set up a user type variable to store the type of user signing up or logging in
+var userType = null;
+
+// Set up redirectUrl to be used with signInSuccessWithAuthResult function below
+var redirectUrl = null;
+
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// Call the getUserType function
+getUserType();
 var uiConfig = {
     callbacks: {
         signInSuccessWithAuthResult: function (authResult, redirectUrl) {
@@ -18,14 +27,22 @@ var uiConfig = {
             var user = authResult.user;
             if (authResult.additionalUserInfo.isNewUser) { //if new user
                 db.collection("Users").doc(user.uid).set({ //write to firestore
-                        Name: user.displayName, //"users" collection
-                        Email: user.email, //with authenticated user's ID (user.uid)
-                    })
+                    Name: user.displayName, //"users" collection
+                    Email: user.email, //with authenticated user's ID (user.uid)
+                    User_Type: userType, //with user type "educator" or "student"
+                })
                     .then(function () {
                         console.log("New user added to firestore");
-                        window.location.assign(
-                            "/html/#.html"
-                        ); //re-direct to #.html after signup
+                        if (userType === "educator") {
+                            window.location.assign(
+                                "/html/educator-new-home.html"
+                            ); //re-direct to educator-new-home.html after signup
+                        } else {
+                            window.location.assign(
+                                "/html/student-new-home.html"
+                            ); //re-direct to student-new-home.html after signup
+                        }
+
                     })
                     .catch(function (error) {
                         console.log("Error adding new user: " + error);
@@ -38,7 +55,7 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: '/html/#.html',
+    signInSuccessUrl: redirectUrl,
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -55,3 +72,17 @@ var uiConfig = {
 };
 // The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
+
+/**
+ * Checks to see which radio button is selected (Educator or Student) and assigns userType to either
+ * "educator" or "student." Also sets redirectUrl to the appropriate page.
+ */
+function getUserType() {
+    if ($("#educator-radio").prop('checked')) {
+        userType = "educator";
+        redirectUrl = "/html/educator-home.html";
+    } else {
+        userType = "student";
+        redirectUrl = "/html/student-home.html";
+    }
+}
