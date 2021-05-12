f3dce9818664c5ef2dfb7f378b3ba00cb62a32dc
Benjie Friedman <benjiefriedman@gmail.com>
2021-05-11 18:51:58 -0700

    If students log in for the first time, their greeting will now say 'Welcome,....' vs. 'Welcome back,....'

--- scripts/hello-student.js
+++ scripts/hello-student.js
@@ -1,6 +1,8 @@
 // JS for personalized greetings (student homepage)
 
-
+// Pull 'firstvisit' tag from URL and use it to choose the correct message to display
+const parsedUrl = new URL(window.location.href);
+var firstVisit = parsedUrl.searchParams.get("firstvisit");
 
 /* Get the current user's name from Firestore and use it to create personalized greetings
    on student-(new-)home.html */
@@ -15,8 +17,11 @@
                     // Extract the first name of the user
                     var name = doc.data().Student_Name.split(" ", 1);
                     if (name) {
-                        $("#personalized-greeting-new-user").html( "Welcome, " + name);
-                        $("#personalized-greeting-established-user").html( "Welcome back, " + name);
+                        if (firstVisit) {
+                            $("#personalized-greeting-new-user").html( "Welcome, " + name);
+                        } else {
+                            $("#personalized-greeting-established-user").html( "Welcome back, " + name);
+                        }
                         // Display a generic message if no name is entered when signing up
                     } else {
                         $("#personalized-greeting-new-user").html("Welcome, GreenQuest User!");
