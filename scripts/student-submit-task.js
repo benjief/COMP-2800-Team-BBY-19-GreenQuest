b5fdd674d379d69cb1830de23341e96cbcabc605
Benjie Friedman <benjiefriedman@gmail.com>
2021-05-12 17:00:09 -0700

    Completed framework for student-submit-task.html (html and css).

--- scripts/student-submit-task.js
+++ scripts/student-submit-task.js
@@ -1,6 +1,24 @@
 // JS for student-submit-task.js
 
 /**
+ * CITE - Implement a character limit counter.
+ * 
+ * @param {*} field - DOM-element that characters are being counted in
+ * @param {*} field2 - ID of the DOM-element displaying the number of characters remaining
+ * @param {*} maxlimit - Maximum number of characters allowed in "field"
+ * @returns - false if the character limit has been exceeded
+ */
+ function charCounter(field, field2, maxlimit) {
+    var countfield = document.getElementById(field2);
+    if (field.value.length > maxlimit) {
+        field.value = field.value.substring(0, maxlimit);
+        return false;
+    } else {
+        countfield.value = maxlimit - field.value.length;
+    }
+}
+
+/**
  * CITE - Displays a picture uploaded by the user in the DOM and resets the "Choose File" input button.
  */
 function processUploadedPicture(){
@@ -12,3 +30,10 @@ function processUploadedPicture(){
     })
 }
 showUploadedPicture();
+
+/**
+ * 
+ */
+function onClickSubmit() {
+
+}
