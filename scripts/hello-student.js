69795d354bc38565bafcc1c7c098cb3546777a86
Samuel Tjahjadi <stjahjadi@my.bcit.ca>
2021-05-17 22:26:08 -0700

    added hello-student.js, logout.js, and student-home.js. student-home.js combines 2 nav buttons into 1.

---
+++ scripts/hello-student.js
@@ -0,0 +1,34 @@
+// JS for personalized greetings (student homepage)
+
+// Pull 'firstvisit' tag from URL and use it to choose the correct message to display
+const parsedUrl = new URL(window.location.href);
+var firstVisit = parsedUrl.searchParams.get("firstvisit");
+
+/* Get the current user's name from Firestore and use it to create personalized greetings
+   on student-(new-)home.html */
+   function sayHello() {
+    firebase.auth().onAuthStateChanged(function (somebody) {
+        if (somebody) {
+            db.collection("Students")
+                .doc(somebody.uid)
+                // Read
+                .get()
+                .then(function (doc) {
+                    // Extract the first name of the user
+                    var name = doc.data().Student_Name.split(" ", 1);
+                    if (name) {
+                        if (firstVisit) {
+                            $("#personalized-greeting").html( "Welcome, " + name);
+                        } else {
+                            $("#personalized-greeting").html( "Welcome back, " + name);
+                        }
+                        // Display a generic message if no name is entered when signing up
+                    } else {
+                        $("#personalized-greeting-new-user").html("Welcome, GreenQuest User!");
+                        $("#personalized-greeting-established-user").html("Welcome back, GreenQuest User!");
+                    }
+                });
+        }
+    });
+}
+sayHello();
---
+++ scripts/student-home.js
@@ -0,0 +1,19 @@
+function onClickStudentHome() {
+    firebase.auth().onAuthStateChanged(function (somebody) {
+        if (somebody) {
+            db.collection("Students")
+                .doc(somebody.uid)
+                // Read
+                .get()
+                .then(function (doc) {
+                    // Extract the current student's quest
+                    hasQuest = doc.data().Student_Quest;
+                    if (hasQuest == true) {
+                        window.location.assign("/html/student-view-quest.html");
+                    } else {
+                        window.location.assign("/html/student-choose-task.html");
+                    }
+                });
+        }
+    });
+}
