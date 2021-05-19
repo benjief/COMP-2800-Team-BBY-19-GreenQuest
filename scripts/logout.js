2b68a3fb7a7adfc9f71de4d18777ec1c8fa414b9
Samuel Tjahjadi <stjahjadi@my.bcit.ca>
2021-05-19 16:56:47 -0700

    added cite description

--- scripts/logout.js
+++ scripts/logout.js
@@ -1,8 +1,10 @@
 // JS for logout
 
 /* User will be logged out upon clicking on the "Log Out" button in the nav bar 
-   As such, they will have to sign in again if they want to have access to the app's core features 
-   https://stackoverflow.com/questions/42571618/how-to-make-a-user-sign-out-in-firebase*/
+   As such, they will have to sign in again if they want to have access to the app's core features
+   
+   I found this code on stackoverflow.com - Our original login functionality didn't work.
+   @see https://stackoverflow.com/questions/42571618/how-to-make-a-user-sign-out-in-firebase*/
 function logout() {
     firebase.auth().signOut().then(function () {
         console.log("Sign-out successful.")
