aac144ac907c5a9d676ccbb74ece65d0fcf84748
Benjie Friedman <benjiefriedman@gmail.com>
2021-05-19 12:31:47 -0700

    Tweaked card backgrounds in about-us.css.

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,22 @@
 // JS for index.html
+
+/**
+ * Write this.
+ * 
+ */
+function getBitmoji() {
+    let storageRef = storage.ref();
+    folderRef = storageRef.child("images/bitmojis");
+    storageRef.child("images/bitmojis/3" + randomNum.toString() + ".png").getDownloadURL()
+        .then((url) => {
+            bitmojiURL = url;
+            // bitmojiURL = imageRef.getDownloadURL();
+            addInfoToDOM();
+        })
+        .catch((error) => {
+            console.error("Error getting url: ", error);
+        })
+        .catch((error) => {
+            console.error("Error getting number of bitmojis: ", error);
+        });
+}
