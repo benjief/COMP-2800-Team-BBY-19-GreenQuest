8bc58086e3e0fd7f990a1d54930b9eb86adb6324
Samuel Tjahjadi <stjahjadi@my.bcit.ca>
2021-05-21 01:21:37 -0700

    Educator-approve-quest/home: Revamped the marking taskflow so it is more efficient for educators. On the marking page where educators approve or reject quests, quests will be cycled through the same page sorted by the data of submission.

--- scripts/educator-home.js
+++ scripts/educator-home.js
@@ -32,7 +32,7 @@ sayHello();
 
 
 // WORK IN PROGRESS
-function hasUnmarkedQuest() {
+function markQuest() {
     if (questIDs.length == 0) {
         console.log("no quest to mark " + questIDs);
         let message = "<div class='text-container'><p class='message'>You haven't got any quests to approve.</p></div>"
@@ -40,59 +40,49 @@ function hasUnmarkedQuest() {
         $("#feedback").show(0);
         $("#feedback").fadeOut(2000);
     } else {
-        console.log("There are quests to mark " + questIDs);
-        let questName = $(event.target).html();
-        let questID = $(event.target).attr("id");
-        setTimeout(function () {
-            location.href = "./educator-approve-quest.html?questname=" + questName + "&questid=" + questID;
-        }, 500);
+        location.href = "./educator-approve-quest.html";
     }
 }
 
-// function listQuests() {
-//     firebase.auth().onAuthStateChanged(function (user) {
-//         if (user) {
-//             db.collection("Educators")
-//                 .doc(user.uid)
-//                 // Read
-//                 .get()
-//                 .then(function (doc) {
-//                     currentUser = doc.id;
-//                     console.log(currentUser);
-//                     getQuests();
-//                 });
-//         }
-//     });
-// }
+/**
+ * Write this
+ */
+ function listQuests() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            db.collection("Educators")
+                .doc(user.uid)
+                // Read
+                .get()
+                .then(function (doc) {
+                    currentUser = doc.id;
+                    console.log(currentUser);
+                    getQuests();
+                });
+        }
+    });
+}
 
-// function getQuests() {
-//     db.collection("Educators").doc(currentUser).collection("Quests")
-//         .orderBy("Date_Submitted", "asc")
-//         .get()
-//         .then((querySnapshot) => {
-//             querySnapshot.forEach((doc) => {
-//                 questIDs.push(doc.id);
-//             });
-//             hasUnmarkedQuest();
-//         })
-//         .catch((error) => {
-//             console.log("Error getting quests: ", error);
-//         });
-// }
+/**
+ * Reads quest IDs from Firestore and puts them into an array.
+ */
+ function getQuests() {
+    db.collection("Educators").doc(currentUser).collection("Quests")
+        .orderBy("Date_Submitted", "asc")
+        .get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                questIDs.push(doc.id);
+            });
+        })
+        .catch((error) => {
+            console.log("Error getting quests: ", error);
+        });
+}
 
-// function onSelectQuest() {
-//     $(document).click(function (event) {
-//         let questName = $(event.target).html();
-//         let questID = $(event.target).attr("id");
-//         setTimeout(function () {
-//             location.href = "./educator-approve-quest.html?questname=" + questName + "&questid=" + questID;
-//         }, 500);
-//     });
-// }
-
-// /**
-//  * Call functions when the page is ready .
-//  */
-// $(document).ready(function () {
-//     listQuests();
-// });
+/**
+ * Call functions when the page is ready .
+ */
+$(document).ready(function () {
+    listQuests();
+});
