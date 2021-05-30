7c7bdcafbcf9dce87b8dda1010e0b7a050d929c8
Benjie Friedman <benjiefriedman@gmail.com>
2021-05-30 03:45:50 -0700

    Got rid of all-pages.js from index.html (was causing issues).

--- scripts/index.js
+++ scripts/index.js
@@ -22,3 +22,28 @@
 // $(document).ready(function () {
 //     getBitmoji();
 // });
+
+/**
+ * Checks the time and changes the background image depending of it's night or day.
+ */
+ function checkTimeOfDay() {
+    let timeOfDay = new Date();
+    if ((20 <= timeOfDay.getHours() && (timeOfDay.getHours() <= 23))
+        || (0 <= timeOfDay.getHours() && timeOfDay.getHours() <= 6)) {
+        $(document.body).css({ backgroundImage: "url('/img/background_stars.png')" });
+        $(".page-heading").css({ color: "white" });
+        $(".student-points-container img").attr("src", "/img/leaf_icon.png");
+        $(".student-points-container p").css({ color: "#ff7b00" });
+    }
+}
+
+/**
+ * Prevents the page from jumping upwards when a user clicks on the buttons.
+ */
+$(".button").click(function (event) {
+    if ($(event.target).attr("href") === "#") {
+        console.log($(event.target).attr("href"));
+        console.log($(event.target).html());
+        event.preventDefault();
+    }
+})
