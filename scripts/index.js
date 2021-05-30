0c1de9e4b973fb61353e47540188ecb6536d1b32
Benjie Friedman <benjiefriedman@gmail.com>
2021-05-30 03:43:24 -0700

    Made index.html image static.

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,24 @@
 // JS for index.html
 
-/**
- * Pulls a bitmoji image from Cloud Storage and pushes it to our index page DOM.
- */
-function getBitmoji() {
-    let storageRef = storage.ref();
-    folderRef = storageRef.child("/images/bitmojis");
-    storageRef.child("/images/bitmojis/36.png").getDownloadURL()
-        .then((url) => {
-            bitmojiURL = url;
-            $("#bitmoji").attr("src", bitmojiURL);
-        })
-        .catch((error) => {
-            console.error("Error getting url: ", error);
-        })
-}
+// /**
+//  * Pulls a bitmoji image from Cloud Storage and pushes it to our index page DOM.
+//  */
+// function getBitmoji() {
+//     let storageRef = storage.ref();
+//     folderRef = storageRef.child("/images/bitmojis");
+//     storageRef.child("/images/bitmojis/36.png").getDownloadURL()
+//         .then((url) => {
+//             bitmojiURL = url;
+//             $("#bitmoji").attr("src", bitmojiURL);
+//         })
+//         .catch((error) => {
+//             console.error("Error getting url: ", error);
+//         })
+// }
 
-/**
- * Calls getBitmoji() when the page is ready.
- */
-$(document).ready(function () {
-    getBitmoji();
-});
+// /**
+//  * Calls getBitmoji() when the page is ready.
+//  */
+// $(document).ready(function () {
+//     getBitmoji();
+// });
