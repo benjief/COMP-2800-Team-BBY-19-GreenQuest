7de645836b3cccf43038ad7ed657a8243d8ad074
Benjie Friedman <benjiefriedman@gmail.com>
2021-05-31 01:40:45 -0700

    Removed all-pages.js from about-use.html.

--- scripts/about-us.js
+++ scripts/about-us.js
@@ -68,9 +68,39 @@ function onClickHome() {
     }
 }
 
+function checkTimeOfDay() {
+    let timeOfDay = new Date();
+    if ((21 <= timeOfDay.getHours() && (timeOfDay.getHours() <= 23))
+        || (0 <= timeOfDay.getHours() && timeOfDay.getHours() <= 4)) {
+        $(document.body).css({ backgroundImage: "url('/img/background_clouds_night.png')" });
+        $(".page-heading").css({ color: "#fff345" });
+        $(".student-points-container img").attr("src", "/img/leaf_icon_night.png");
+        $(".student-points-container p").css({ color: "#fff345" });
+    } else if ((19 <= timeOfDay.getHours() && (timeOfDay.getHours() < 21))
+        || (4 < timeOfDay.getHours() && timeOfDay.getHours() <= 7)) {
+        $(document.body).css({ backgroundImage: "url('/img/background_clouds_dusk_dawn.png')" });
+        $(".page-heading").css({ color: "white" });
+        $(".student-points-container img").attr("src", "/img/leaf_icon.png");
+        $(".student-points-container p").css({ color: "#ff7b00" });
+    }
+}
+
 /**
- * Calls randomizeBackground() when the page is ready.
+ * Prevents the page from jumping upwards when a user clicks on the buttons.
+ */
+$(".button").click(function (event) {
+    if ($(event.target).attr("href") === "#") {
+        console.log($(event.target).attr("href"));
+        console.log($(event.target).html());
+        event.preventDefault();
+    }
+})
+
+/**
+ * Calls randomizeBackground(), checkLoggedIn(), checkTimeOfDay() when a page is ready.
  */
 $(document).ready(function () {
     randomizeBackground();
+    checkLoggedIn();
+    checkTimeOfDay();
 });
